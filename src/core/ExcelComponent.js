29ee003578e745db64d43ba02377ce14fd2eebf9
xifamox <pavelmager2006@mail.ru>
2023-11-10 18:15:15 +0300

    Finish redux and table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,7 +5,10 @@ export class ExcelComponent extends DOMListener {
     super($root, options.listeners)
     this.name = options.name || ''
     this.emitter = options.emitter
+    this.subscribe = options.subscribe || []
+    this.store = options.store
     this.unsubscribes = []
+    this.storeSub = null
 
     this.prepare()
   }
@@ -27,6 +30,17 @@ export class ExcelComponent extends DOMListener {
   $on(event, fn) {
     const unsub = this.emitter.subscribe(event, fn)
     this.unsubscribes.push(unsub)
+  }
+
+  $dispatch(action) {
+    this.store.dispatch(action)
+  }
+
+  // Сюда приходят только изменения по тем полям, на которые мы подписались
+  storeChanged() {}
+
+  isWatching(key) {
+    return this.subscribe.includes((key))
   }
 
   // Инициализируем компонент
